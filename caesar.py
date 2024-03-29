e631019f4e5ff1bcf2cd168331b01bc8e0246cdc
OutmaAixLara <162890536+OutmaAixLara@users.noreply.github.com>
2024-03-29 18:52:53 +0100

    fixed string index out of range

--- caesar.py
+++ caesar.py
@@ -11,12 +11,12 @@ for i in range(n):
     c = str_in[i]
     if c in alphabets_maj:
         loc = alphabets_maj.find(c)
-        newLoc = loc + 3
+        newLoc = (loc + 3)%26
         str_out += alphabets_maj[newLoc]
         print(loc, c, newLoc, str_out)
     elif c in alphabets_min:
         loc = alphabets_min.find(c)
-        newLoc = loc + 3
+        newLoc = (loc + 3)%26
         str_out += alphabets_min[newLoc]
         print(loc, c, newLoc, str_out)
     else:
